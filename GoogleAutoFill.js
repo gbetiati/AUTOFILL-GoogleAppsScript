90043e2e5d57158c6b3f97b504f525666b801b33
Guilherme Augusto Betiati Heusser <108565148+gbetiati@users.noreply.github.com>
2023-11-03 14:50:56 -0300

    Update GoogleAutoFill.js
    Adição de funcionalidades
    1. Quem preenche o formulario pode escolher detalhes de cabecalho e/ou o qual arquivo template vai ser gerado, tudo através do menu de opções do formulário Google;
    2. O colaborador que preenche o form também preencherá seus dados e esses dados são enviados automaticamente a uma planilha que contabiliza quanto cada colaborador gerou e exibe gráficos conforme os dados recebidos, como nome colaborador, tipo do documento cadastrado, captação ou campanha de origem do cliente.

--- GoogleAutoFill.js
+++ GoogleAutoFill.js
@@ -1,84 +1,114 @@
-function ContratcFunction(e) {
+function CallFunction(e) {
+  ProcFunction(e);
+  useDataRange(e);
+}
+
+function ProcFunction(e) {
   
     var Timestamp = e.values[0];
-    var CabecalhoUm = e.values[1];
-    var Nome = e.values[3]
-    var Nacionalidade = e.values[3];
-    var Profissao = e.values[4];
+    var Nome = e.values[1];
+    var Nacionalidade = e.values[2]
+    var EstadoCivil = e.values[3];
+    var Cpf = e.values[4];
     var Rg = e.values[5]
-    var Cpf = e.values[6];
-    var Email = e.values[7];
-    var EstadoCivil = e.values[8];
-    var NomeConj = e.values[10];
-    var Nacionalidadeconj = e.values[11];
-    var CabecalhoDois = e.values[12];
-    var ProfissaoConj = e.values[13];
-    var RGConj = e.values[14];
-    var CPFConj = e.values[15];
-    var EmailConj = e.values[16];
-    var Rua = e.values[17];
-    var Bairro = e.values[18];
-    var Cidade = e.values[19];
-    var UF = e.values[20];
-    var CEP = e.values[21];
-    var ComiC1 = e.values[22];
-    var ComiC1Text = e.values[23];
-    var ComiC2 = e.values[24];
-    var ComiC2Text = e.values[25];
-    var ComiC3 = e.values[27];
-    var ComiC3Text = e.values[28];
-    var DataPagC1 = e.values[30];
-    var ContaC1 = e.values[31];
-    var ContaC3 = e.values[34];
-    var DataAssContrato = e.values[35];
-    var AssinaturaUm = e.values[36];
-    var AssinaturaDois = e.values[37];
-    var AssinaturaTres = e.values[38];
-  
-    var TemplateDocu = DriveApp.getFileById("2nk6RPxv...7b6J329gWedM");
-    var PastaContratos = DriveApp.getFolderById("1m1S5V...qQ42h7Ow");
-  
-    var Copi = TemplateDocu.makeCopy(Nome, PastaContratos); 
-        var Documento = DocumentApp.openById(Copi.getId());
-        var body = Documento.getBody();
-        var foooter = Documento.getFooter();
-  
-  
-      body.replaceText("{{cabecalhocorretorvendedor1}}",CabecalhoUm);
-      body.replaceText("{{cabecalhocorretorvendedor2}}",CabecalhoDois);
-      body.replaceText("{{nomeclientevendedor}}",Nome);
-      body.replaceText("{{naccliente}}",Nacionalidade);
-      body.replaceText("{{profissaocliente}}",Profissao);
-      body.replaceText("{{rgcliente}}",Rg);
-      body.replaceText("{{cpfvendedor}}",Cpf);
-      body.replaceText("{{emailcliente}}",Email);
-      body.replaceText("{{estadocivilcliente}}",EstadoCivil);
-      body.replaceText("{{nomeconje}}",NomeConj );
-      body.replaceText("{{nacconje}}",Nacionalidadeconj);
-      body.replaceText("{{profissaoconje}}",ProfissaoConj);
-      body.replaceText("{{rgconje}}",RGConj);
-      body.replaceText("{{cpfconje}}",CPFConj);
-      body.replaceText("{{emailconje}}",EmailConj);
-      body.replaceText("{{ruacliente}}",Rua);
-      body.replaceText("{{bairrocliente}}",Bairro);
-      body.replaceText("{{cidadecliente}}", Cidade);
-      body.replaceText("{{ufcliente}}",UF);
-      body.replaceText("{{cepcliente}}", CEP);
-      body.replaceText("{{comiimobi}}",ComiC1);
-      body.replaceText("{{comiimobitext}}",ComiC1Text);
-      body.replaceText("{{datapgtocomi}}",DataPagC1);
-      body.replaceText("{{comicorretor1}}",ComiC2);
-      body.replaceText("{{comicorretor1text}}",ComiC2Text);
-      body.replaceText("{{datapgtocomi}}",DataPagC1);
-      body.replaceText("{{ctabancáriacorretor1}}",ContaC1);
-      body.replaceText("{{comicorretor2}}",ComiC3);
-      body.replaceText("{{comicorretor2text}}",ComiC3Text);
-      body.replaceText("{{datapgtocomi}}",DataPagC1);
-      body.replaceText("{{ctabancáriacorretor2}}",ContaC3);
-      body.replaceText("{{datacontrato}}",DataAssContrato);
-      body.replaceText("{{asscorretor1}}",AssinaturaUm);
-      body.replaceText("{{asscorretor2}}",AssinaturaDois);
-      body.replaceText("{{asscorretor3}}",AssinaturaTres);
-  
-      Documento.saveAndClose();
+    var Profissao = e.values[6];
+    var Rua = e.values[7];
+    var Numero = e.values[8];
+    var Bairro = e.values[9];
+    var Cidade = e.values[10];
+    var Cep = e.values[11];
+    var Telefone = e.values[12];
+    var Email = e.values[13];
+    var Data = e.values[14];
+    var TipoAcao = e.values[16];
+    var Peticionante = e.values[18];
+   
+    var NeydianneKit = 'Dra....';
+
+    var LaisKit = 'Dra...';
+
+    var BrendaKit = 'Dra...';
+    
+    
+    if (TipoAcao=='APOSENTADORIA POR INVALIDEZ' || TipoAcao=='AUX.DOENÇA' || TipoAcao=='AUX.ACIDENTE') {
+      var urlTemplate = ("152EmgdmyTXtBSTaw5I2_5llLQewxrZhurY-cWrfKJ2w");
+    }else if (TipoAcao=='CONTAGEM DE TEMPO' ||TipoAcao=='REVISÃO BENEFICIO' || TipoAcao=='CONCESSÃO     APOSENTADORIA') {
+      var urlTemplate = ("1N92jNrB-2c3LTQk8");
+    }else {
+      var urlTemplate = ("1NKotqS9grYVTjLfqvIvxc");
+    }Ws
+    
+    var TemplateDocu = DriveApp.getFileById(urlTemplate);
+    var PastaDest = DriveApp.getFolderById("1ymBLodt1OMr3YmsgCtgFF");
+
+    var Copi = TemplateDocu.makeCopy(Nome, PastaDest); 
+
+    var Procuracao = DocumentApp.openById(Copi.getId());
+
+    var body = Procuracao.getBody();
+
+    var foooter = Procuracao.getFooter();
+
+    body.replaceText("{{nome}}",Nome);
+    body.replaceText("{{nac}}",Nacionalidade);
+    body.replaceText("{{estadocivil}}",EstadoCivil);
+    body.replaceText("{{cpf}}",Cpf);
+    body.replaceText("{{rg}}",Rg);
+    body.replaceText("{{profissao}}",Profissao);
+    body.replaceText("{{rua}}",Rua);
+    body.replaceText("{{numero}}",Numero);
+    body.replaceText("{{bairro}}",Bairro);
+    body.replaceText("{{cidade}}",Cidade);
+    body.replaceText("{{cep}}",Cep);
+    body.replaceText("{{telefone}}",Telefone);
+    body.replaceText("{{email}}",Email);
+    body.replaceText("{{data}}",Data);
+    body.replaceText("{{tipoação}}", TipoAcao);
+
+
+  if (Peticionante== 'NEYDIANNE') {
+    body.replaceText("{{peticionante}}",NeydianneKit );
+    } else if (Peticionante=='LAIS') {
+      body.replaceText("{{peticionante}}",LaisKit );
+    } else if (Peticionante=='BRENDA') {
+      body.replaceText("{{peticionante}}",BrendaKit );
   }
+
+Procuracao.saveAndClose();      
+}
+
+var url = "https://docs.google.com/spreadsheets"
+
+function useDataRange (e) {
+
+  var planilhabase = SpreadsheetApp.openByUrl(url);
+  var guiadados = planilhabase.getSheetByName('DADOS')
+
+  var planilhamenu =  SpreadsheetApp.getActiveSpreadsheet();
+  var guiamenu = planilhamenu.getSheetByName('MENU');
+
+  var ultimalinha = guiamenu.getLastRow();
+
+  var area = "P:Q" + ultimalinha;
+  var dados = guiamenu.getRange(area).getValues();
+    
+  var linhadados = guiadados.getLastRow() + 1;
+
+  var areaData = "A" + ultimalinha
+  var data = guiamenu.getRange(areaData).getValues();
+
+  guiadados.getRange(linhadados, 1,dados.length, dados[0].length).setValues(dados);
+  guiadados.getRange(linhadados, 3, data.length, data[0].length).setValues(data);
+
+  guiamenu.getRange(area);
+  guiamenu.getRange("A2").activate();
+
+  guiamenu.getRange(areaData);
+  guiamenu.getRange("A2").activate();
+
+    Logger.log(areaData)
+
+  Browser.msgBox('salvo com sucesso')
+
+}
+
